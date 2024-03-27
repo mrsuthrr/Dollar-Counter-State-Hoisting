ded88f2f643f409f9db5f1a1b07a9d55dcfa803e
Deepak Suthar <deepak.marwad@gmail.com>
2024-03-28 00:50:51 +0530

    Done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.dollarcounterjack"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.dollarcounterjack"
--- app/src/main/java/com/example/dollarcounterjack/MainActivity.kt
+++ app/src/main/java/com/example/dollarcounterjack/MainActivity.kt
@@ -3,13 +3,34 @@ package com.example.dollarcounterjack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dollarcounterjack.ui.theme.DollarCounterJackTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,15 +39,71 @@ class MainActivity : ComponentActivity() {
         setContent {
             DollarCounterJackTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
+}
+
+
+@Composable
+fun MyApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        DollarCounter()
+    }
+}
+
+@Composable
+fun DollarCounter() {
+
+    val counter = remember {
+        mutableStateOf(1)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "$${counter.value * 100}", style = MaterialTheme.typography.headlineSmall)
+
+        Spacer(modifier = Modifier.height(180.dp))
+
+        CustomButton { counter.value++ }
+    }
+}
+
+
+@Composable
+fun CustomButton(onClick: () -> Unit) {
+
+    ElevatedCard(
+        modifier = Modifier
+            .size(120.dp)
+            .clickable {
+                onClick.invoke()
+            }, shape = RoundedCornerShape(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Yellow, //Card background color
+            contentColor = Color.Black  //Card content color,e.g.text
+        )
+    ) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(
+                text = "Tap",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+
+    }
+
 }
 
 @Composable
